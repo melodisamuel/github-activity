9fe45b8acf07a214338a1c683deaefd1a1942dff
Melody <melodysamuelcodes@gmail.com>
2025-01-02 15:05:56 +0100

    # Process and display data

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import axos from "axios";
+import axios from "axios";
 import { Command } from "commander";
 
 interface GithubEvent {
@@ -15,5 +15,16 @@ program
   .version("1.0.0");
 
   program.command('activity <username>').description('Fetch recent activity of a github user').action(async (username: string) => {
-    
+
+    const url = `https://api.github.com/users/${username}/events/public`;
+
+    try {
+        console.log(`Fetching recent activity for user: ${username}...\n`);
+
+        const response = await axios.get<GithubEvent[]>(url);
+
+        if(response.data.length === 0) {
+            console.log('No recent acivity found.');
+        }
+    }
   })
